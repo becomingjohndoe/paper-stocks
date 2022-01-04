edeed9c5d2e7edd3cf9c08936567fe37ff9c2669
becomingjohndoe <karl.swatman@gmail.com>
2022-01-04 14:44:47 +0000

    adds cost basis

--- src/components/BuySell.jsx
+++ src/components/BuySell.jsx
@@ -1,10 +1,33 @@
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
-const BuySell = ({ buyAllStock, sellAllStock }) => {
+import Change from "./Change";
+
+const BuySell = ({
+	buyAllStock,
+	sellAllStock,
+	stocksBought,
+	costBasis,
+	priceInfo,
+}) => {
 	const [buyOrSell, setBuyOrSell] = useState(true);
 
 	return (
 		<div className="buy-sell-container">
+			<div className="portfolio__container">
+				<div className="stocks">shares owned</div>
+				<div className="stocks">{stocksBought.toFixed(2)} </div>
+			</div>
+
+			<div className="portfolio__container">
+				<div className="money">cost basis</div>
+				<div className="money"> {costBasis.toFixed(2)}</div>
+				<div
+					className="percent-change"
+					style={{ color: priceInfo.costBasisIsUp ? "green" : "red" }}
+				>
+					{`(${priceInfo.costBasisPercentChange.toFixed(2)}%)`}
+				</div>
+			</div>
 			{buyOrSell ? (
 				<div className="portfolio__container__buy">
 					<Button
