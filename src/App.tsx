f2f4fd980b9e7870db1d704f6a1cfe23029df6af
becomingjohndoe <karl.swatman@gmail.com>
2022-01-11 13:17:24 +0000

    update to typescript

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./components/Header";
+import Portfolio from "./components/Portfolio";
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+function App() {
+	const [showModal, setShowModal] = useState(false);
+	return (
+		<div className="App">
+			<Container>
+				<Header />
+				<Portfolio />
+			</Container>
+		</div>
+	);
+}
+
+export default App;
